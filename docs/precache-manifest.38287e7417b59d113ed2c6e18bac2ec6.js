ef8b47dd0e290a308314aee10094c5719ab75f89
Yad Smood <y.s.inside@gmail.com>
2018-06-05 21:14:51 +0900

    build

---
+++ docs/precache-manifest.38287e7417b59d113ed2c6e18bac2ec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b295ca0236a766556131",
+    "url": "/js/chunk-vendors.b295ca02.js"
+  },
+  {
+    "revision": "4c6cfe4705a68d602301",
+    "url": "/js/app.4c6cfe47.js"
+  },
+  {
+    "revision": "a483eb5722b3c052de17d2e5d9e862c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b295ca0236a766556131",
+    "url": "/css/chunk-vendors.3745e6c3.css"
+  },
+  {
+    "revision": "4c6cfe4705a68d602301",
+    "url": "/css/app.70270403.css"
+  }
+];
